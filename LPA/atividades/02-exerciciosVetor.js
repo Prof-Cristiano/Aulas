1ec72f6d469fc299d1ec37c3f695543addd57e64
Prof. Cristiano <prof.cristianodepaula@gmail.com>
2026-03-18 08:11:14 -0300

    Adiciona exercícios de funções em JavaScript, incluindo cálculo de idade, distância percorrida e operações básicas

--- LPA/atividades/02-exerciciosVetor.js
+++ LPA/atividades/02-exerciciosVetor.js
@@ -7,3 +7,27 @@
     5. Exclua o elemento uva.
     6. Crie uma cópia do array apenas com os elementos com índices 2, 3 e 4.
 */
+
+//Crie um array de frutas com os seguintes elementos: Banana, Maçã, Pera, Uva, Morango.
+var frutas = ["Banana", "Maçã", "Pera", "Uva", "Morango"];
+console.table(frutas);
+
+// Adicione Tangerina no final
+frutas.push("Tangerina");
+console.table(frutas);
+
+// Adicione Goiaba no início
+frutas.unshift("Goiaba");
+console.table(frutas);
+
+// Exiba o conteúdo do índice 5
+console.log(frutas[5]);
+
+// Exclua o elemento uva
+var posicao = frutas.indexOf("Uva");
+frutas.splice(posicao, 1);
+console.table(frutas);
+
+// Crie uma cópia do array apenas com os elementos com índices 2, 3 e 4
+var copia = frutas.slice(2, 5);
+console.table(copia);
---
+++ LPA/atividades/03-exerciciosFuncoes.js
@@ -0,0 +1,28 @@
+/*****************************************************
+Lista de Exercícios
+*****************************************************/
+/*
+1. Crie uma função que receba como parâmetro o ano de nascimento de uma pessoa a retorne sua idade.
+
+2. Crie uma função que receba a quantidade de combustível de um carro e retorne quantos Kms ele pode viajar com esse combustível. Considere que o
+ veículo faz uma média de 12 Km/L.
+*/
+
+// 1. Crie uma função que receba como parâmetro o ano
+// de nascimento de uma pessoa a retorne sua idade.
+function idade(anoAtual, anoNascimento) {
+  return anoAtual - anoNascimento;
+}
+console.log("A pessoa tem", idade(2026, 2001), "anos.");
+
+// 2. Crie uma função que receba a quantidade de combus-
+// tível de um carro e retorne quantos Kms ele pode
+// viajar com esse combustível. Considere que o
+// veículo faz uma média de 12 Kms/L.
+function distancia(combustivel) {
+  const desempenho = 12; // 12 Kms por litro
+  return combustivel * desempenho;
+}
+console.log("Carro 1: 20L -> Percorre", distancia(20), "Kms.");
+console.log("Carro 2: 8L -> Percorre", distancia(8), "Kms.");
+console.log("Carro 3: 2L -> Percorre", distancia(2), "Kms.");
